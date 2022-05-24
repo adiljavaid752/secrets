031b5937f8900ac4f6956b3a172e18598cb6f3b7
adil javed <adiljavaid752@gmail.com>
2022-05-24 16:09:43 +0530

    hash and salting

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+gi
+const session = require('express-session')
+
 
 
 
 const app = express();
-const encrypt= require("mongoose-encryption")
+
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 app.set("view engine", "ejs");
@@ -17,13 +20,14 @@ app.use(
     extended: true,
   })
 );
-app.use(express.static("public"));
+app.use(express.static("public")); 
+const saltRounds=10
 const userSchema = new mongoose.Schema({
   email: String,
   passwrod: String,
 });
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields: ['password']})
+
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function (req, res) {
@@ -37,29 +41,39 @@ app.get("/register", function (req, res) {
   res.render("register");
 });
 app.post("/register", function (req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password,
-  });
-  newUser.save(function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+});
+  
+  
 });
 app.post("/login", function (req, res) {
+  
   const username = req.body.username;
   const possword = req.body.password;
+
   User.findOne({ email: username }, function (err, foundUser) {
     if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === possword) {
-          res.render("secrets");
-        }
+        bcrypt.compare(possword, foundUser.password, function(err, result) {
+
+          if( result === true){
+            result.render("secrets")
+          }
+      });
+        
       }
     }
   })
